ff16673ee09547ac7b2656846098d66941fe9158
nana <nana@C037519400K0>
2018-08-08 13:50:19 +0800

    test

--- src/main/java/nana_qa/maven_demo/DiffDemo.java
+++ src/main/java/nana_qa/maven_demo/DiffDemo.java
@@ -19,11 +19,11 @@ public class DiffDemo {
 //
 //	}
 	
-	public String getDiff(String str1,String str2) throws Exception {
-		if(StringUtils.isEmpty(str1)||StringUtils.isEmpty(str2)){
+	public String getDiff(String str3,String str2) throws Exception {
+		if(StringUtils.isEmpty(str3)||StringUtils.isEmpty(str2)){
 			throw new Exception("Empty or Null String is not allowed.");
 		}
-		return StringUtils.difference(str1, str2);
+		return StringUtils.difference(str3, str2);
 	}
 	
 	
